a5501bc6b3b494602f149777d698cafdde55baf1
kimuit117 <kemoikiuita@gmail.com>
2022-11-28 00:40:55 +0300

    completed task

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -2,14 +2,17 @@ class CheesesController < ApplicationController
 
   # GET /cheeses
   def index
-    cheeses = Cheese.all
-    render json: cheeses
+  cheeses = Cheese.all
+  render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+def show
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    render json: { error: 'Cheese not found' }
   end
-
+end
 end
